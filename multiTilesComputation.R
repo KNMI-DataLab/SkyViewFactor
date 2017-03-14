5bdbc6877188c9b0666066f67dbb44c339ac970e
Andrea Pagani <pagani@birdexp06.knmi.nl>
2017-03-14 14:26:39 +0000

    test

--- multiTilesComputation.R
+++ multiTilesComputation.R
@@ -70,8 +70,8 @@ checkIfMultiTile <- function(path, coordX, coordY){
   file<-list.files(path = path, pattern = paste0("ahn_", coordX,"_",coordY,"_1.*"), full.names = T, recursive = T)
   if (length(file)!=0){
     multifile<-TRUE}
-  multifile
-}
+  multifile 
+}  
 
 
 makeSpatialDF <- function(df,projection){
