73a499118dd25fe7f369c4056f76543508ff6dfc
Andrea Pagani <pagani@birdexp06.knmi.nl>
2017-03-14 13:29:50 +0000

    back to a working multitile after yesterday crash

--- multiTilesComputation.R
+++ multiTilesComputation.R
@@ -32,92 +32,25 @@ mainTile<-makeSpatialDF(mainTile,projection = pro)
 extensionMainTile<-extent(mainTile)
 
 neighbors<-loadNeighborTiles(lazFolder, tileNumberXCoord, tileNumberYCoord, extensionMainTile, maxView, pro)
+#neighbors<-loadNeighborTilesTest(lazFolder, tileNumberXCoord, tileNumberYCoord, extensionMainTile, maxView, pro)
 
-resterizedNeighbors<-lapply(neighbors, makeRaster, xres, yres)
-mergedNeighbors<-lapply(rasterizedNeighbors, merge)
-rasterizedMainTile<-makeRaster(mainTile,xres,yres)
-
-
-fileToLoad<-paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord,".laz")
-tileNeighborsRight<-c(paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord-1000,".laz"),paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord,".laz"), paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord+1000,".laz"))
-tileNeighborsLeft<-c(paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord-1000,".laz"),paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord,".laz"), paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord+1000,".laz"))
-tileNeighborsUpDown<-c(paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord-1000,".laz"),paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord+1000,".laz"))
-
-filesToLoad<-c(tileNeighborsLeft,tileNeighborsRight,tileNeighborsUpDown,fileToLoad)
-
-paths<-list()
-for(file in filesToLoad){
-  
-  print(file)
-  fileLoc<-list.files(path = "/run/media/pagani/knmi/ahn2_clean/tileslaz", pattern = file,recursive = T, full.names = T)    
-  paths<-c(paths,fileLoc)
-}
-
-print(paths)
-
-
-
-#bash commands
-#system("echo $AHN")
-#system("cp $AHN /nobackup/users/dirksen/SVF_highres/SVF/test/LAS/$AHNname.laz")
-
-## way to unzip them all
-lapply(paths,file.copy,to="data/tiles/")
-currentFiles<-list.files(path = "data/tiles/", full.names = TRUE)
-lapply(paste(lasZipLocation, currentFiles), system)
-#system("/usr/people/pagani/opt/testFile/LAStools/bin/laszip .laz")
-system("rm data/tiles/*.laz")
-
-file_las<-list.files("data/tiles",pattern="*.las$",full.names = TRUE)
-# file_las<-file_las[1]
-
-centralFile<-paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord,".las")
-
-centralFilePath<- list.files("data/tiles",pattern=centralFile, full.names=TRUE)
-
-centralTile<-readLAS(centralFilePath)
-centralTile<-data.frame(centralTile)
-
-coordinates(centralTile)<-~X+Y
-proj4string(centralTile)<-pro
-
-maxView<-100
-
-centralTileInfo<-extent(centralTile)
-
-out.matrix<-lapply(file_las, readLAS)
-out<-do.call(rbind.data.frame, out.matrix)
-#out<-data.frame(out.matrix)
-
-rm(out.matrix)
-coordinates(out)<-~X+Y
-proj4string(out)<-pro
 
 xres<-5 # x-resolution in meters
 yres<-5 # y-resolution in meters
 
-r_test<-raster(nrow=10,ncol=10,crs=pro) #dummy raster with projection
-extent(r_test)<-extent( centralTile)+2*maxView #changed to the extent of the central tile plus the radius # same extent as the las file
-res(r_test)<-c(xres,yres) # set the resolution
 
-out<-crop(out,extent(r_test))
 
-r<-rasterize(out,r_test,field="Z") #rasterizing the spatial points to a 1x1 grid
+rasterizedNeighbors<-lapply(neighbors, makeRaster, xres, yres, pro)
+mergedNeighbors<-do.call(merge, rasterizedNeighbors)
+rasterizedMainTile<-makeRaster(mainTile,xres,yres,pro)
+fullRaster<-merge(rasterizedMainTile, mergedNeighbors)
 
-rm(out)
+r.svf<-svf(fullRaster, nAngles=16, maxDist= maxView, ll=F)
+out<-crop(r.svf,extent(rasterizedMainTile))
 
-###
-r.AHN3<-r
-
-###
-r.svf<-svf(r, nAngles=16, maxDist= maxView, ll=F)
-
-##reducing the grid to just the central part after computing the svf with the extra bound
-extent(r.svf)<-extent(centralTile)
-extent(r)<-extent(centralTile)
 
 # plot(r.svf)
-r.b<-brick(r,r.svf)
+r.b<-brick(rasterizedMainTile,r.svf)
 names(r.b)<-c("Z","SVF")
 r.df<-as.data.frame(r.b,xy=TRUE)
 
@@ -232,8 +165,79 @@ loadNeighborTiles <- function(path,tileNumberXCoord, tileNumberYCoord, extension
   dfs<-c(df1,df2,df3,df4,df5,df6,df7,df8)
 }
 
-makeRaster<-function(spatialDf, xres, yres){
+
+
+loadNeighborTilesTest <- function(path,tileNumberXCoord, tileNumberYCoord, extensionMainTile, maxView,projection){
+  print("hello")
+  #tileNeighborsLeftLower<-paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord-1000,".laz")
+  tileNeighborsLeftLowerX<-tileNumberXCoord-1000
+  tileNeighborsLeftLowerY<-tileNumberYCoord-1000
+  df<-loadTile(path,tileNeighborsLeftLowerX,tileNeighborsLeftLowerY)
+  df<-makeSpatialDF(df,projection)
+  df1<-crop(df,c(xmin(extensionMainTile)-maxView,xmin(extensionMainTile),ymin(extensionMainTile)-maxView, ymin(extensionMainTile)))
   
+  #tileNeighborLeft<-paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord,".laz")
+  tileNeighborsLeftX<-tileNumberXCoord-1000
+  tileNeighborsLeftY<-tileNumberYCoord
+  df<-loadTile(path,tileNeighborsLeftX,tileNeighborsLeftY)
+  df<-makeSpatialDF(df,projection)
+  df2<-crop(df,c(xmin(extensionMainTile)-maxView,xmin(extensionMainTile),ymin(extensionMainTile), ymax(extensionMainTile)))
+  
+  # #tileNeighborLeftUpper<-paste0("ahn_", tileNumberXCoord-1000,"_",tileNumberYCoord+1000,".laz")
+  # tileNeighborsLeftUpperX<-tileNumberXCoord-1000
+  # tileNeighborsLeftUpperY<-tileNumberYCoord+1000
+  # df<-loadTile(path,tileNeighborsLeftUpperX,tileNeighborsLeftUpperY)
+  # df<-makeSpatialDF(df,projection)
+  # df3<-crop(df,c(xmin(extensionMainTile)-maxView,xmin(extensionMainTile),ymax(extensionMainTile), ymax(extensionMainTile)+maxView))
+  # 
+  # #tileNeighborsRightLower<-paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord-1000,".laz")
+  # tileNeighborsRightLowerX<-tileNumberXCoord+1000
+  # tileNeighborsRightLowerY<-tileNumberYCoord-1000
+  # df<-loadTile(path,tileNeighborsRightLowerX,tileNeighborsRightLowerY)
+  # df<-makeSpatialDF(df,projection)
+  # df4<-crop(df,c(xmax(extensionMainTile),xmax(extensionMainTile)+maxView,ymin(extensionMainTile)-maxView, ymin(extensionMainTile)))
+  # 
+  # #tileNeighborRight<-paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord,".laz")
+  # tileNeighborsRightX<-tileNumberXCoord+1000
+  # tileNeighborsRightY<-tileNumberYCoord
+  # df<-loadTile(path,tileNeighborsRightX,tileNeighborsRightY)
+  # df<-makeSpatialDF(df,projection)
+  # df5<-crop(df,c(xmax(extensionMainTile),xmax(extensionMainTile)+maxView,ymin(extensionMainTile), ymax(extensionMainTile)))
+  # 
+  # #tileNeighborRightUpper<-paste0("ahn_", tileNumberXCoord+1000,"_",tileNumberYCoord+1000,".laz")
+  # tileNeighborsRightUpperX<-tileNumberXCoord+1000
+  # tileNeighborsRightUpperY<-tileNumberYCoord+1000
+  # df<-loadTile(path,tileNeighborsRightUpperX,tileNeighborsRightUpperY)
+  # df<-makeSpatialDF(df,projection)
+  # df6<-crop(df,c(xmax(extensionMainTile),xmax(extensionMainTile)+maxView,ymax(extensionMainTile), ymax(extensionMainTile)+maxView))
+  # 
+  # #tileNeighborsCentralDown<-paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord-1000,".laz")
+  # tileNeighborsCenterDownX<-tileNumberXCoord
+  # tileNeighborsCenterDownY<-tileNumberYCoord-1000
+  # df<-loadTile(path,tileNeighborsCenterDownX,tileNeighborsCenterDownY)
+  # df<-makeSpatialDF(df,projection)
+  # df7<-crop(df,c(xmin(extensionMainTile),xmax(extensionMainTile),ymin(extensionMainTile)-maxView, ymin(extensionMainTile)))
+  # 
+  # #tileNeighborsCentralUp<-paste0("ahn_", tileNumberXCoord,"_",tileNumberYCoord+1000,".laz")
+  # tileNeighborsCenterUpX<-tileNumberXCoord
+  # tileNeighborsCenterUpY<-tileNumberYCoord+1000
+  # df<-loadTile(path,tileNeighborsCenterUpX,tileNeighborsCenterUpY)
+  # df<-makeSpatialDF(df,projection)
+  # df8<-crop(df,c(xmin(extensionMainTile),xmax(extensionMainTile),ymax(extensionMainTile), ymax(extensionMainTile)+maxView))
+  # 
+  dfs<-c(df1,df2)
+}
+
+
+
+
+
+
+
+
+
+
+makeRaster<-function(spatialDF, xres, yres, pro){
   dummyRaster<-raster(nrow=10,ncol=10,crs=pro) #dummy raster with projection
   extent(dummyRaster)<-extent(spatialDF)
   res(dummyRaster)<-c(xres,yres) # set the resolution
