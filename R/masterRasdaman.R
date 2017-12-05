1822f1f33cd7109d08b33e771f28bb1c3d1756b8
andreapago <andrea.pago@gmail.com>
2017-12-05 16:53:17 +0000

    some bugs fixed, still y has to be checked

--- R/masterRasdaman.R
+++ R/masterRasdaman.R
@@ -25,6 +25,9 @@ xDistance<-abs(upperCorner[[1]][[1]]-lowerCorner[[1]][[1]])
 Xmax<-max(upperCorner[[1]][[1]],lowerCorner[[1]][[1]])
 Xmin<-min(upperCorner[[1]][[1]],lowerCorner[[1]][[1]])
 
+Ymax<-max(upperCorner[[1]][[2]],lowerCorner[[1]][[2]])
+Ymin<-min(upperCorner[[1]][[2]],lowerCorner[[1]][[2]])
+
 processingTileSideX<-xDistance/100
 
 radiusSVF<-100
@@ -37,10 +40,10 @@ processingTileSideY<-slaveBand/10
   
 
 
-coverageExample <- GET(paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X(140000,141000)&subset=Y(456200,458200)&format=image/tiff"))
+#coverageExample <- GET(paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X(140000,141000)&subset=Y(456200,458200)&format=image/tiff"))
 
 
-contentTIFF<-content(coverageExample,"raw")
+#contentTIFF<-content(coverageExample,"raw")
 
 
 foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", "uuid"),
@@ -58,29 +61,38 @@ foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", 
                 
                 numTilesX<-xDistance/processingTileSideX 
                 numTilesY<-slaveBand/processingTileSideY
-                for(yside=1:numTilesY){
+                
+                for(yside in 1:numTilesY){
                   if (yP==Ymin){
                     ySelLow = Ymin
+                    ySelHigh = ySelLow + processingTileSideY + radiusSVF
                   } else{
-                    ySelLow = yInitial + processingTileSideY*yside -radiusSVF
+                    ySelLow = yInitial + processingTileSideY*(yside-1) -radiusSVF
+                    ySelHigh = ySelLow + processingTileSideY + 2*radiusSVF
                   }
-                  ySelHigh = ySelLow + processingTileSideY + radiusSVF
-                  if(ySelHigh>yMax){
-                    ySelHigh = yMax
+                  if(ySelHigh>Ymax){
+                    ySelHigh = Ymax
                   }
-                  for(xside=1:numTilesX){
+                  for(xside in 1:2){#numTilesX){
                     if(xP==Xmin){
                       xSelLow = Xmin
+                      xSelHigh = xSelLow + processingTileSideX + radiusSVF
                     } else{
-                      xSelLow = xInitial + processingTileSideX*xside -radiusSVF
+                      xSelLow = xInitial + processingTileSideX*(xside-1) -radiusSVF
+                      xSelHigh = xSelLow + processingTileSideX + 2*radiusSVF
                     }
-                    xSelHigh = xSelLow + processingTileSideX + radiusSVF
-                    if(xSelHigh>xMax){
-                      xSelHigh = xMax
+                    
+                    if(xSelHigh>Xmax){
+                      xSelHigh = Xmax
                     }
-                    coverageExample <- GET(paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X("xSelLow","xSelHigh,")&subset=Y("ySelLow","ySelHigh")&format=image/tiff"))
+                    print(c(xP, yP))
+                    coverageExample <- GET(paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X(",xSelLow,",",xSelHigh,")&subset=Y(",ySelLow,",",ySelHigh,")&format=image/tiff"))
+                    print(coverageExample)
+                    xP =  xInitial + processingTileSideX*(xside)
+                    
                     
                   }
+                  yP =  yInitial + processingTileSideY*(yside)
                 }
               }
 
