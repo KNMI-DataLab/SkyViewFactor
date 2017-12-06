181525c91277e4d3be4d6d18490f262a7f0cf980
andreapago <andrea.pago@gmail.com>
2017-12-06 13:41:26 +0000

    changed to a working elaboration of SVF using wget command and cleaning -Inf values and putting as NA

--- rscripts/masterRasdaman.R
+++ rscripts/masterRasdaman.R
@@ -3,6 +3,11 @@ library(foreach)
 library(doParallel)
 library(httr)
 library(tiff)
+library(raster)
+library(rgdal)
+library(rtiff)
+library(horizon)
+
 
 
 #master operation
@@ -46,10 +51,16 @@ processingTileSideY<-slaveBand/10
 #contentTIFF<-content(coverageExample,"raw")
 
 
-foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", "uuid"),
-  .export = c("loadTile", "checkMultiTile", "makeSpatialDF", "loadNeighborTiles","makeRaster",
-              "pro", "workingPath", "lazFolder", "lasZipLocation", "maxView", "Xres", "Yres",
-              "loadNeighborTile_v2","mergeNeighborTiles"), .combine = f(length(listTiles))) %do%{
+#foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", "uuid"),
+ # .export = c("loadTile", "checkMultiTile", "makeSpatialDF", "loadNeighborTiles","makeRaster",
+   #           "pro", "workingPath", "lazFolder", "lasZipLocation", "maxView", "Xres", "Yres",
+   #           "loadNeighborTile_v2","mergeNeighborTiles"), .combine = f(length(listTiles))) %dopar%{
+    i=1            
+                
+                Xmin=140000
+                Ymin=456000
+                
+                
                 xInitial = Xmin
                 
                 ySlaveBand<-i * slaveBand
@@ -57,6 +68,8 @@ foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", 
                 
                 xP<-xInitial
                 yP<-yInitial
+                
+                
               
                 
                 numTilesX<-xDistance/processingTileSideX 
@@ -73,7 +86,7 @@ foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", 
                   if(ySelHigh>Ymax){
                     ySelHigh = Ymax
                   }
-                  for(xside in 1:2){#numTilesX){
+                  for(xside in 1:8){#numTilesX){
                     if(xP==Xmin){
                       xSelLow = Xmin
                       xSelHigh = xSelLow + processingTileSideX + radiusSVF
@@ -86,13 +99,31 @@ foreach(i =  1:numSlaves, .packages = c("raster", "horizon", "rgdal", "rLiDAR", 
                       xSelHigh = Xmax
                     }
                     print(c(xP, yP))
-                    coverageExample <- GET(paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X(",xSelLow,",",xSelHigh,")&subset=Y(",ySelLow,",",ySelHigh,")&format=image/tiff"))
-                    print(coverageExample)
-                    xP =  xInitial + processingTileSideX*(xside)
+                    coverageExample <- paste0("http://",host,":8080/rasdaman/ows?service=WCS&version=2.0.1&request=GetCoverage&coverageId=",coverageId,"&subset=X(",xSelLow,",",xSelHigh,")&subset=Y(",ySelLow,",",ySelHigh,")&format=image/tiff")
                     
+                    command<-paste0("wget \"", coverageExample, "\" -O temp.tiff")
+                    
+                    system(command)
+                    
+                    #print(coverageExample)
+                    xP =  xInitial + processingTileSideX*(xside)
+                    #tiffRaw<-coverageExample$content
+                    #tiffByte<-readTIFF(tiffRaw)
+                    #writeTiff(tiffByte,"temp.tiff")
+                    rastertest<-raster("temp.tiff")
+                    #crs(rastertest)<-CRS("+init=epsg:28992")
+                    #extent(rastertest)<-extent(xSelLow,xSelHigh,ySelLow,ySelHigh)
+                    info<-GDALinfo(fname = "temp.tiff")
+                    #print(tiffByte)
+                    rastertest <- reclassify(rastertest, c(-Inf, -1000,NA))
+                    rastertest<-aggregate(rastertest, fact = 20)
+                    rasterSVF<-svf(rastertest, nAngles = 16, maxDist = 100, ll = F)
+                    plot(rastertest)
+                    plot(rasterSVF)
                     
                   }
+                  xP = xInitial
                   yP =  yInitial + processingTileSideY*(yside)
                 }
-              }
+             # }
 
