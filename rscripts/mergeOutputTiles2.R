b916f2cd8f341857bc0927158cf22f8a6eb0bc39
Ubuntu <ubuntu@ip-172-31-12-107.us-east-2.compute.internal>
2018-01-14 10:47:58 +0000

    last changes for merging

--- rscripts/mergeOutputTiles2.R
+++ rscripts/mergeOutputTiles2.R
@@ -12,7 +12,7 @@ library(parallel)
 library(SpaDES)
 
 output_dir<<-"/home/ubuntu/efs/output/SVF_1m/"
-splits_dir<<-"/home/ubuntu/efs/output/SVF_1m_regions/"
+splits_dir<<-"/home/ubuntu/efs/output/SVF_1m_regionsNew/"
 
 
 files<-list.files(output_dir,full.names = T, pattern = "*.gri")
@@ -24,17 +24,23 @@ rasterOutput<-function(x){
   rr
 }
 
+
+rasterOptions(maxmemory=460e9)
+message("building cluster")
 cl<-makeCluster(61, type = "FORK")
 
 
-
+message("select the layer and getting the raster from every tile")
 wholeRasterList<-parLapply(cl,files,rasterOutput)
 stopCluster(cl)
+
 #rasterOptions(tolerance = 100)
 #options(overlap=F)
+
 message("starting merging the tiles")
 totalRaster<-do.call(merge, c(wholeRasterList, list(tolerance=100)))
 message("full raster merged, now split in 16 regions")
+#totalRaster<-raster("/home/ubuntu/efs/output/tempRastersCopy/r_tmp_2018-01-10_213252_2546_27069.grd")
 sections<-splitRaster(totalRaster,4,4,path =splits_dir)
 
 
