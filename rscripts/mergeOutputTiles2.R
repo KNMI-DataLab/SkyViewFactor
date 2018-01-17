6cc77d618cf48580dd3441b0a7b61537d284410e
andreapago <andrea.pago@gmail.com>
2018-01-17 17:25:23 +0000

    added conversion to NetCDF according format for KNMI Data Centrum visualization and CF-1.4

--- rscripts/mergeOutputTiles2.R
+++ rscripts/mergeOutputTiles2.R
@@ -10,6 +10,7 @@ library(stringr)
 library(spatial.tools)
 library(parallel)
 library(SpaDES)
+library(ncdf4)
 
 output_dir<<-"/home/ubuntu/efs/output/SVF_1m/"
 splits_dir<<-"/home/ubuntu/efs/output/SVF_1m_regionsNew/"
@@ -61,13 +62,54 @@ convertToNetCDF<-function(file){
   writeRaster(ras,gsub(".gri",".nc",file))
 }
 
+changesToNetCDFFileForKDC<-function(netCDFFile){
+  
+  ncFile<-nc_open(netCDFFile)
+  x<-ncvar_get(ncFile,'easting')
+  y<-ncvar_get(ncFile,'northing')
+  coordsProjString<-ncatt_get(ncFile, 0, attname ='crs')
+  svf<-ncvar_get(ncFile,"layer")
+  
+  
+  
+  xD <- ncdim_def( "x", "m", x)
+  yD <- ncdim_def( "y", "m", y)
+  #xVar<-ncvar_def("x","projection_x_coordinate",dim = xD)
+  #yVar<-ncvar_def("y","projection_y_coordinate",dim = yD)
+  svfVar<-ncvar_def("svf","",list(xD,yD), missval = -3.4e+38, longname = "sky_view_factor", prec = 'float')
+  coordVar<-ncvar_def("projection","",dim = list(), prec = 'char')
+  prodVar<-ncvar_def("product", "", dim = list(), prec="char")
+  
+  newNC<-nc_create(gsub(".nc","_kdcVers.nc",netCDFFile),list(svfVar,coordVar,prodVar))
+  
+  ncvar_put(newNC,xVar,x)
+  ncvar_put(newNC,yVar,y)
+  ncvar_put(newNC,svfVar,svf)
+  
+  ncatt_put(newNC, varid = svfVar, attname = "grid_mapping", attval = "projection")
+  ncatt_put(newNC, varid = "x", attname = "standard_name", attval = "projection_x_coordinate")
+  ncatt_put(newNC, varid = "y", attname = "standard_name", attval = "projection_y_coordinate")
+  ncatt_put(newNC, varid = prodVar, attname = "date_start_of_data", attval = "20070101T000000")
+  ncatt_put(newNC, varid = prodVar, attname = "date_end_of_data", attval = "20130101T000000")
+  ncatt_put(newNC, varid = coordVar, attname = 'proj4_params', attval = coordsProjString$value, prec = "char")
+  ncatt_put(newNC, varid = 0, attname = "Conventions", attval = "CF-1.4")
+  ncatt_put(newNC, varid = 0, attname = "comment", attval = "Sky view factor based on the AHN2 height dataset")
+  
+  nc_close(newNC)
+ 
+  
+
+}
 
 
 
+#main
 rasterOptions(maxmemory=20e9)
-cl<-makeCluster(12, type = "FORK")
+cl<-makeCluster(16, type = "FORK")
 parLapply(cl, files,convertToNetCDF)
-stopCluster()
+filesNC<-list.files(output_dir,full.names = T, pattern = "*.nc")
+parLapply(cl,filesNC,changesToNetCDFFileForKDC)
+stopCluster(cl)
 
 
 
