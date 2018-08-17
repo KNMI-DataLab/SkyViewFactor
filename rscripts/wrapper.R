a0d6841ac21f807f079a0ec21ed7c10ed798958d
andreapago <andrea.pago@gmail.com>
2018-08-17 08:35:32 +0000

    small log

--- rscripts/wrapper.R
+++ rscripts/wrapper.R
@@ -2,6 +2,6 @@ source("rscripts/allTilesComputation.R")
 
 loginfo("preparing cluster")
 cl<-prepareCluster()
-loginfo("Cluter ready for use")
+loginfo("Cluster ready for use")
 loginfo("Calling main function")
 main()
